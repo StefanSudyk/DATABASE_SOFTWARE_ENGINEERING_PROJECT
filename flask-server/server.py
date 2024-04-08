e6f4f745985961c77c4a7ab915738e12e6559911
Damian Świeca <damianswieca04@gmail.com>
2024-04-08 14:40:06 +0200

    Fixed whole project stucture
    Flask + react app template

--- flask-server/server.py
+++ flask-server/server.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/members")
 def members():
     return {"members": ["Member1", "Member2", "Member3"]}
+
 
 if __name__ == "__main__":
     app.run(debug=True)
