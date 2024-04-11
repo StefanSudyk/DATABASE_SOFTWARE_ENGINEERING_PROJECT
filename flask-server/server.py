6311bfa7e3a7858a4be7b156f0d0758f92135cb3
Kamil Uchwat <92688578+Hirocco@users.noreply.github.com>
2024-04-11 10:20:18 +0200

    Tworzenie tabeli, inicjowanie połączenia z BD
    Rozszerzyłem projekt o dwie biblioteki pymysql wspiera dodatkowo połączenie sqlalchemy z mysql oraz biblitoekę do kryptografii haseł etc. 
    Stworzona została inicjalna tabela user.
    W komentarzu w plikach jest wszystko krok po kroku jak zrobić.

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,12 +1,37 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+"""
+Ustawienia w mysql workbench zróbcie tak by były podobne do moich. 
+Po odpaleniu connectora zróbcie poniższe w terminalu:
 
-@app.route("/members")
-def members():
-    return {"members": ["Member1", "Member2", "Member3"]}
+cd flask-server
+ python
+    >>>from app import app
+    >>>from app import db
+    >>>db.create_all()
+    
+To odpali i stworzy wam przez pythona pierwszą tabele, która znajduje sie poniżej.
+"""
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:password@localhost/housedb"
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+app.app_context().push()
+
+class User(db.Model):
+    id = db.Column(db.Integer, unique=True, primary_key=True)
+    name = db.Column(db.String(80))
+    surname = db.Column(db.String(80))
+    phone_number = db.Column(db.String(9))
+
+    def __init__(self, name, surname, phone_number):
+        self.name = name
+        self.surname = surname
+        self.phone_number = phone_number
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
