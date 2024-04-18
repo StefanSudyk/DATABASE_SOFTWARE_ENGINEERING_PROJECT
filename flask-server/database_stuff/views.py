714328b643a0ba17051850d1272e5e97d1369e96
swiczka <damianswieca04@gmail.com>
2024-04-18 20:05:24 +0200

    added templates, sign up, login, etc, web pages
    prepared model html pages for testing db with forms to fill:
    you can log in (with session included)
    you can log out
    you can sign up
    you can fill company info

--- flask-server/database_stuff/views.py
+++ flask-server/database_stuff/views.py
@@ -1,6 +1,17 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, url_for, redirect
+from auth import session
 
-views = Blueprint('view', __name__)
+views = Blueprint('views', __name__, static_folder="static", template_folder="templates")
+
 @views.route('/')
-def home():
-    return "<h1>TEST</h1>"
+def index():
+    return render_template('index.html')
+
+
+@views.route('/user')
+def user():
+    if "usr_phnum" in session:
+        phnum = session["usr_phnum"]
+        return render_template('user.html', phone_number=phnum)
+    else:
+        return redirect(url_for("auth.login"))
