53b3510d0562db660f5e15fa33ccb867fee4fb62
Filip Stępień <154385462+stepienn@users.noreply.github.com>
2024-04-20 21:08:24 +0200

    Setting html site with navbar
    shotout to local history in VSC code

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from '../src/Components/Navbar/Navbar.jsx'
+
+const App = () => {
+  return (
+    <div className='container'>
+      <Navbar/>
+      </div>
+  )
+}
+
+export default App;
