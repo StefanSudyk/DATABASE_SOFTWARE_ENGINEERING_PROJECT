70fce9dbcc9e34e473a24e7b63e3252208ac3ee2
Filip Stępień <154385462+stepienn@users.noreply.github.com>
2024-04-25 20:22:51 +0200

    Added working routing, and pages, and isLoggedIn

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import Navbar from '../src/Components/Navbar/Navbar.jsx'
-import { BrowserRouter } from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 import WyswtlApi from './Components/Pages/WyswtlApi.jsx';
+import Zaloguj from './Components/Pages/Zaloguj.jsx'
+import Wyloguj from './Components/Pages/Wyloguj.jsx'
+import Strona_glowna from './Components/Pages/Strona_glowna.jsx'
+
 
 const App = () => {
+  const isLoggedIn = false;
   return (
+     
     <BrowserRouter>
-    <Navbar/>
-    <div className='background'/>
-      
-      
+    <Navbar isLoggedIn={isLoggedIn}/>
+    <Routes>
+      <Route path="/" element={<Strona_glowna/>} />
+      <Route path="Test_API" element={<WyswtlApi/>} />
+      <Route path="Zaloguj" element={<Zaloguj/>}/>
+      <Route path="Wyloguj" element={<Wyloguj/>}/>
+    </Routes>
+    
       </BrowserRouter>
   )
 }
