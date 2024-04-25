87bf1035b2b45fe9e4c38114d2bd33cb45c88855
StefanSudyk <141357825+StefanSudyk@users.noreply.github.com>
2024-04-25 21:00:59 +0200

    Config files

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import main_logo from '../../assets/main_logo.png'
 /*import ogloszenie from '../../assets/Ogłoszenie.png' */
 import {Link} from "react-router-dom"
 
-const Navbar = () => {
+const Navbar = ({ isLoggedIn }) => {
   return (
     
     <div className='navbar'>
@@ -15,12 +15,15 @@ const Navbar = () => {
           <li className="navbar_text_test_api">
             <Link to="/Test_API">Testowanie API</Link>
           </li>
-
-        <li className="navbar_text">
-        <Link to="/Logowanie">Zaloguj/Zarejestruj</Link>
-        </li>
-        
-        <li className='DodajOgloszenie'> Dodaj ogłoszenie</li>
+          {isLoggedIn ? (
+            <Link to="/Wyloguj">Wyloguj</Link>
+          ) : (
+            <Link to="/Zaloguj">Zaloguj</Link>
+          )}
+     
+       
+        <li className='DodajOgloszenie'>
+          <Link to="/Dodaj_ogloszenie"> Dodaj ogłoszenie </Link></li>
 
         {/*<img src={ogloszenie}  className='logo'/> nie uzyte zdjecie, jako przycisk*/}
       </ul>
