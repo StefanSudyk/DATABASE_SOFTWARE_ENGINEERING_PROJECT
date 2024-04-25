dc57c286139f60f566be65e081352a06bf6ef3de
StefanSudyk <stefs2812@gmail.com>
2024-04-25 21:00:59 +0200

    Config files

---
+++ client/src/Components/Pages/Dodaj_ogloszenie.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Dodaj_ogloszenie = () => {
+  return (
+    <div>
+       <div>
+        dodaj ogloszenie
+      </div>
+      
+      <div>
+        dodaj ogloszenie
+      </div>
+      
+      <div>
+        dodaj ogloszenie
+      </div>
+      
+      <div>
+        dodaj ogloszenie
+      </div>
+      
+      <div>
+        dodaj ogloszenie
+      </div>
+      
+    </div>
+  )
+}
+
+export default Dodaj_ogloszenie
