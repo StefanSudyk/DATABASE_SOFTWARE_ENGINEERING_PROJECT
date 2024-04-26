46ab382edceab4401d8000f3dc6a562cd3004fa1
kubek1608 <164409912+kubek1608@users.noreply.github.com>
2024-04-26 21:19:37 +0200

    form
    form

---
+++ client/src/Components/Pages/Dodaj_ogloszenie/Dodaj_ogloszenie.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Form } from './Form'
+const Dodaj_ogloszenie = () => {
+  return (
+    <center>
+    <><><div className='nawigfill'>a</div></>
+    <h1>DODAWANIE OGLOSZENIA!</h1>
+    <Form/>
+    </>
+    </center>
+  )
+}
+
+export default Dodaj_ogloszenie
---
+++ client/src/Components/Pages/Zaloguj/Zaloguj.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Form } from './Form'
+import { FormRej } from './FormRej'
+const Zaloguj = () => {
+  return (
+    <div>
+      <div>
+        a
+      </div>
+      <div>
+        zaloguj
+      </div>
+      <Form/>
+      <FormRej/>
+    </div>
+  )
+}
+
+export default Zaloguj
