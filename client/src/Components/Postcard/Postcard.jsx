fd36bb4c4d75ef4474364c0651397b78d41610d7
StefanSudyk <stefs2812@gmail.com>
2024-04-28 13:22:35 +0200

    Postcard component fix with animation zoom out after mouse hover

--- client/src/Components/Postcard/Postcard.jsx
+++ client/src/Components/Postcard/Postcard.jsx
@@ -6,9 +6,9 @@ import wroclaw  from '../../assets/wroclaw-photo.jpg'
 import poznan from '../../assets/poznan-photo.jpg'
 import gdansk from '../../assets/gdansk-photo.jpg'
 
-
 const postcard = () => {
   return (
+    
     <div className='postcard-main-box'>
       <div className='writing-name'>
           <div className='popular-location-title'>
@@ -21,55 +21,58 @@ const postcard = () => {
       <div className="main-photos-box">
         <div className='first-row-postcard'>
           <div className='photo-warsaw'>
-              <div className='box-warsaw'>
+              <div className='box-photo-up-row'>
               <div className='text-photos'>
                   Warszawa
                 </div>
-                <picture>
+                  <a href='https://www.youtube.com/'>
                   <img src={warsaw} alt="Warszawa" className='picture-up-row' />
-                </picture>
+                  </a>
+               
               </div>
           </div>
           <div className='photo-wroclaw'>
-              <div className='box-wroclaw'>
+              <div className='box-photo-up-row'>
               <div className='text-photos'>
                   Wrocław
               </div>
-                <picture>
+                <a href=''>
                   <img src={wroclaw} alt="Wrocław" className='picture-up-row'/>
-                </picture>
+                </a>
               </div>
           </div>
         </div>
       <div className="second-row-postcard">
-         <div className='photo-gdansk'>
-              <div className='box-gdansk'>
-              <div className='text-photos'>
+        {/* Tutaj powinno byc photo-gdansk, ale nie wiem dlaczego photo-gdansk nie działa w css jak to jest ten sam kod więc mamy 
+        photo-krakow, ponieważ to działa*/}
+          <div className='photo-krakow'>  
+              <div className='box-photo-down-row'>
+                <div className='text-photos'>
                   Gdańsk
-              </div>  
-                <picture>
-                  <img src={gdansk} alt="Gdańsk" className='picture-down-row'/>
-                </picture>
+                </div>
+                  <a href=''>
+                  <img src={gdansk} alt="Gdańsk" role='presentation' className='picture-down-row' />
+                  </a>
               </div>
           </div>
           <div className='photo-poznan'>
-              <div className='box-poznan'>
+              <div className='box-photo-down-row'>
               <div className='text-photos'>
                   Poznań
               </div>  
-                <picture>
+                  <a href=''>
                   <img src={poznan} alt="Poznań" className='picture-down-row'/>
-                </picture>
+                  </a>
               </div>
           </div>
           <div className='photo-cracow'>
-              <div className='box-cracow'>
+              <div className='box-photo-down-row'>
                 <div className='text-photos'>
                   Kraków
                 </div>
-                <picture>
+                  <a href=''>
                   <img src={cracow} alt="Kraków" role='presentation' className='picture-down-row' />
-                </picture>
+                  </a>
               </div>
           </div>
         </div>
