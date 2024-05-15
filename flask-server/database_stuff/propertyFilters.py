e1ee6fc4073e18b1a845f596f569bef44f824617
DanielWajd <wajdowiczdaniel@gmail.com>
2024-05-15 11:07:01 +0200

    Fixed Property, updated propertyFilters

--- flask-server/database_stuff/propertyFilters.py
+++ flask-server/database_stuff/propertyFilters.py
@@ -1,1 +1,8 @@
 from models import Property
+
+
+#filtrowanie po cenie - wyswietla tylko properties z zakresu ceny
+
+def filter_by_price(price_from, price_to):
+    properties = Property.query.filter(Property.price.between(price_from, price_to)).all()
+    return properties
