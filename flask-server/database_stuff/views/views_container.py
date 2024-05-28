eb1da574c3d96036267b338f61187a10fc04a6a6
swiczka <damianswieca04@gmail.com>
2024-05-28 21:29:33 +0200

    login try 2137
    DOES NOT WORK DO NOT USE

--- flask-server/database_stuff/views/views_container.py
+++ flask-server/database_stuff/views/views_container.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template, url_for, redirect
 from auth1 import session
+from flask_login import current_user
+import requests
 
 views = Blueprint('views', __name__, static_folder="static", template_folder="templates")
 
@@ -10,8 +12,13 @@ def index():
 
 @views.route('/user')
 def user():
-    if "phonenumber" in session:
-        phonenumber = session["phonenumber"]
+    response = requests.get("http://127.0.0.1:5000/currentuser")
+    print("sprobuj no")
+    print(response)
+    print(type(response))
+        
+    if current_user.is_authenticated:
+        phonenumber = current_user.phone_number
         return render_template('user.html', phone_number=phonenumber)
     else:
         return redirect(url_for("auth.login"))
