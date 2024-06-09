37d600c417c59c2e6261e771a4b7bcde16f936c1
StefanSudyk <stefs2812@gmail.com>
2024-06-09 18:00:05 +0200

    confirmationPopUp fix

--- client/src/Components/Pages/User_profile/confirmationPopUp.jsx
+++ client/src/Components/Pages/User_profile/confirmationPopUp.jsx
@@ -1,17 +1,21 @@
-// ConfirmationPopup.jsx
 import React from 'react';
 import './confirmationPopUp.css';
 
 function ConfirmationPopup({ isOpen, onConfirm, onClose }) {
+
   if (!isOpen) {
     return null;
   }
 
   return (
+    <div className='popup-background'>
     <div className="confirmation-popup">
-      <h2>Are you sure you want to delete this property?</h2>
-      <button onClick={onConfirm}>Delete</button>
-      <button onClick={onClose}>Close</button>
+      <p className=''>Are you sure you want to delete this property?</p>
+      <div className='confirmation-button-container'>
+      <button className='confirmation-notification-button' onClick={onClose}>Cancel</button>
+      <button className='confirmation-notification-button' onClick={onConfirm}>Confirm</button>
+      </div>
+    </div>
     </div>
   );
 }
