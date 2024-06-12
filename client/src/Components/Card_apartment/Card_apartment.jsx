6b75c0ae2c9ba88ea0a9c6897cc25504d9432529
StefanSudyk <stefs2812@gmail.com>
2024-06-12 23:00:44 +0200

    Add link path to our property

--- client/src/Components/Card_apartment/Card_apartment.jsx
+++ client/src/Components/Card_apartment/Card_apartment.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Card_apartment.css';
 import photo_template from '../../../src/assets/card_template.png';
 import './DaneDoWczytania'
 
 
 const Card_apartment = (props) => {
-  const { NazwaOkolicy, CenaMieszkania, IloscMetrow,CenaMetrow, Miasto, Zdjecie } = props;
+  const { property_id, NazwaOkolicy, CenaMieszkania, IloscMetrow,CenaMetrow, Miasto, Zdjecie } = props;
+
+  console.log(property_id)
 
   return (
+    <Link to={`/property/${property_id}`} className='link-card-apartment'>
     <div className='card_apartment_calosc' >
     <div className='card_apartment_block'>
       <div className='inside_card'>
@@ -15,15 +19,14 @@ const Card_apartment = (props) => {
           <div className='Tekst_gorny'>{NazwaOkolicy}, {Miasto}</div>
           <div className='Tekst_dolny'>{CenaMieszkania} PLN &nbsp;  {IloscMetrow} m²&nbsp;&nbsp; {CenaMetrow} PLN/m2 </div>
           
-          
-         
         </div>
-          
+       
         <img src={Zdjecie} className='photo_card_template' alt="Zdjęcie nieruchomości" />
-        
+      
       </div>
     </div>
     </div>
+    </Link>
   );
 };
 
