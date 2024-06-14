e7d86959e68fdf5c697f214b920e50dcfdf2517c
DanielWajd <wajdowiczdaniel@gmail.com>
2024-06-14 23:30:38 +0200

    Filtry zrobione i próba

--- client/src/Components/Pages/Strona_glowna/SearchRes/SearchResults.jsx
+++ client/src/Components/Pages/Strona_glowna/SearchRes/SearchResults.jsx
@@ -1,22 +1,50 @@
-// components/SearchResults.js
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import '../SearchRes/SearchResults.css'
+import '../SearchRes/SearchResults.css';
+import DanePoFiltracji from '../../../Card_apartment/DanePoFiltracji.jsx';
 
 function useQuery() {
   return new URLSearchParams(useLocation().search);
 }
 
 const SearchResults = () => {
-  const query = useQuery().get('query');
+  const query = useQuery();
+  const [propertiesData, setPropertiesData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  // Tutaj dodaj logikę wyszukiwania i wyświetlania wyników na podstawie query
+  useEffect(() => {
+    fetchProperties(query);
+  }, [query]);
+
+  const fetchProperties = (queryParams) => {
+    const params = new URLSearchParams(queryParams);
+
+    // Dodaj dodatkowe parametry jeśli są potrzebne
+    // params.append('additional_param', 'value');
+
+    const url = `http://127.0.0.1:5000/getallproperty?${params.toString()}`;
+
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        setPropertiesData(data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error('Error fetching data: ', error);
+        setLoading(false);
+      });
+  };
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className="results-container">
       <div className="left-side">
-        <h1>Results for: {query}</h1>
-        {/* Tutaj dodaj logikę wyszukiwania i wyświetlania wyników na podstawie query */}
+        <h1>Results for: {query.toString()}</h1>
+        <DanePoFiltracji propertiesData={propertiesData} />
       </div>
       <div className="right-side">
         <h1>Additional Information</h1>
