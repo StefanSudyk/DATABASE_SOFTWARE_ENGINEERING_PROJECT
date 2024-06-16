bc1c53f6bb67e2ca0a354dc4c54ae83363f084be
StefanSudyk <stefs2812@gmail.com>
2024-06-16 08:45:35 +0200

    error fix

--- client/src/Components/Card_apartment/DaneDoWczytania.jsx
+++ client/src/Components/Card_apartment/DaneDoWczytania.jsx
@@ -25,20 +25,21 @@ const DaneDoWczytania = () => {
   
   return (
     <>
-      {propertiesData.map(propertyData =>{
-        console.log('property_id:', propertyData.property.id_property)
+      {propertiesData.map(propertyData => {
+        console.log('property_id:', propertyData.property.id_property); // Added semicolon here
         return (
-        <Card_apartment 
-          key={propertyData.property.id_property}
-          property_id={propertyData.property.id_property}
-          NazwaOkolicy={propertyData.address.county} 
-          CenaMieszkania={propertyData.property.price}
-          IloscMetrow={propertyData.property.square_metrage}
-          Miasto={propertyData.address.locality} 
-          CenaMetrow={propertyData.property.p_p_meter}
-          Zdjecie={`data:image/png;base64, ${propertyData.photos.length > 0 ? propertyData.photos[0].photo : ''}`}
-        />
-      ))}
+          <Card_apartment 
+            key={propertyData.property.id_property}
+            property_id={propertyData.property.id_property}
+            NazwaOkolicy={propertyData.address.county} 
+            CenaMieszkania={propertyData.property.price}
+            IloscMetrow={propertyData.property.square_metrage}
+            Miasto={propertyData.address.locality} 
+            CenaMetrow={propertyData.property.p_p_meter}
+            Zdjecie={`data:image/png;base64, ${propertyData.photos.length > 0 ? propertyData.photos[0].photo : ''}`}
+          />
+        );
+      })}
     </>
   );
   
